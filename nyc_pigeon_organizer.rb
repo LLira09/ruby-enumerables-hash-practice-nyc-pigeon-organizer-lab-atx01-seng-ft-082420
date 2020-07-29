8bfd95d151dc8a545eabef9f88d9b66a70ab17ef
Jake <llira09@gmail.com>
2020-07-29 18:50:02 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
     value.each do |inner_key, names|
       names.each do |name|
         if !final_array[name]
-          final_array[name] ={}
+          final_array[name] = {}
       end
     end 
     final_array
